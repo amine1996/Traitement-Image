24c47ce1b1888b76464c36fcb14a71f524087a21
ISMAIL AMine <amine_ismail@hotmail.fr>
2017-09-15 11:42:44 +0200

    Prend seuil, input et output en parametre et fait un treshhold

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -2,36 +2,34 @@
 #include <stdio.h>
 #include <iostream>
 
-
-
-
-
-
 using namespace std;
 using namespace cv;
 
+int main(int argc, char **argv)
+{
+	if (argc < 4)
+	{
+		cout << "Utilisation : ./seuilgris seuil image_entree image_binaire_sortie" << std::endl;
+		return -1;
+	}
 
+	const int seuil = atoi(argv[1]);
+	const char *input = argv[2];
+	const char *output = argv[3];
 
-int main(int argc, char** argv){
+	Mat grayscaleInput = imread(input, CV_LOAD_IMAGE_GRAYSCALE);
+	Mat binaryOutput(grayscaleInput.size(), grayscaleInput.type());
 
+	if (!grayscaleInput.data) // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
 
+	threshold(grayscaleInput, binaryOutput, seuil, 255, THRESH_BINARY);
 
-	Mat imageIn = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-	
-	
-	if(! imageIn.data )                              // Check for invalid input
-    	{
-        	cout <<  "Could not open or find the image" << std::endl ;
-        	return -1;
-   	}
-	
-       
-       
-	
-	imshow( "Display window", imageIn );             
-	waitKey(0);       	
+	imshow("Display window", binaryOutput);
+	waitKey(0);
 
-	
 	return 0;
 }
-
