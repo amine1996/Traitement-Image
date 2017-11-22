8f19d89b846ffacee913e8c2e51d72c08f70154b
ISMAIL AMine <amine_ismail@hotmail.fr>
2017-11-22 19:56:27 +0100

    ex 1 fini, ex 2 buggé

--- TP3/fast.cpp
+++ TP3/fast.cpp
@@ -7,130 +7,197 @@
 using namespace std;
 using namespace cv;
 
-/**
- *Sobel Function 
- */
-Mat sobel(Mat grayscaleInput, bool gaussian)
+struct cornerPoint
 {
-	Mat grayscaleCopy = grayscaleInput.clone();
+	Point point;
+	bool matched;
+};
 
-	if (gaussian)
-		GaussianBlur(grayscaleCopy, grayscaleCopy, Size(9, 9), 0, 0);
+vector<cornerPoint> getCorners(Mat grayscalePic)
+{
+	//Delta for a 3x3 circle around the point of interest
+	const vector<int> vecDeltaX = {0, 1, 2, 3, 3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1};
+	const vector<int> vecDeltaY = {3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1, 0, 1, 2, 3};
+	vector<cornerPoint> vecCorners;
 
-	Mat output(grayscaleCopy.size(), grayscaleCopy.type());
-	Mat gradx(grayscaleCopy.size(), grayscaleCopy.type());
-	Mat grady(grayscaleCopy.size(), grayscaleCopy.type());
-
-	Sobel(grayscaleCopy, gradx, -1, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-	Sobel(grayscaleCopy, grady, -1, 0, 1, 3, 1, 0, BORDER_DEFAULT);
-
-	double max = 0;
-	for (int i = 0; i < grayscaleCopy.rows; i++)
+	for (int col = 0; col < grayscalePic.cols; col++)
 	{
-		for (int j = 0; j < grayscaleCopy.cols; j++)
+		for (int row = 0; row < grayscalePic.rows; row++)
 		{
-			uchar value = (uchar)round(sqrt(pow(gradx.at<uchar>(i, j), 2) + pow(grady.at<uchar>(i, j), 2)));
-			output.at<uchar>(i, j) = value;
-			if (max < value)
-				max = value;
-		}
-	}
+			//Value of the point of interest
+			uchar valCenterPixel = grayscalePic.at<uchar>(row, col);
 
-	max /= 4;
-	threshold(output, output, max, 255, THRESH_BINARY);
-
-	return output;
-}
-
-/**
- *Grey scale Function 
- */
-Mat seuilDeGris(Mat grayscaleInput, uchar seuil)
-{
-	Mat output(grayscaleInput.size(), grayscaleInput.type());
-	threshold(grayscaleInput, output, seuil, 255, THRESH_BINARY_INV);
-	return output;
-}
-
-int main(int argc, char **argv)
-{
-	if (argc < 3)
-	{
-		cout << "Utilisation : ./fast image_entree image_sortie" << endl;
-		return -1;
-	}
-
-	const char *input = argv[1];
-	const char *output = argv[2];
-
-	Mat originalPic = imread(input, CV_LOAD_IMAGE_COLOR);
-	Mat grayscaleInput = imread(input, CV_LOAD_IMAGE_GRAYSCALE);
-	Mat sobel_(grayscaleInput.size(), grayscaleInput.type());
-
-	if (!grayscaleInput.data || !originalPic.data) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << endl;
-		return -1;
-	}
-
-	vector<int> vecDeltaX = {0, 1, 2, 3, 3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1};
-	vector<int> vecDeltaY = {3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1, 0, 1, 2, 3};
-
-	vector<Point2d> vecCorners;
-
-	for (int row = 0; row < grayscaleInput.rows; row++)
-	{
-		for (int col = 0; col < grayscaleInput.cols; col++)
-		{
-			uchar valCenterPixel = grayscaleInput.at<uchar>(col, row);
-
+			//Value of the delta of the compared pixel from the point of interest
 			int deltaX;
 			int deltaY;
+
+			//Delta to be brighter/darker than the point of interest
 			int deltaVal = 15;
 
+			//Number of contiguous brighter pixels
 			int nbContiguousBrighter = 0;
+
+			//Number of contiguous darker pixels
 			int nbContiguousDarker = 0;
-			int neededContiguous = 9;
 
-			bool foundCorner = false;
+			//Number of needed contiguous pixels to be a corner
+			const int neededContiguous = 20;
 
-			for (int k = 0; k < vecDeltaX.size() + 8; k++)
+			//Looking for a circle and a half around the point of interest
+			for (unsigned int k = 0; k < vecDeltaX.size() + 8; k++)
 			{
+				//Position of the compared pixel
 				deltaX = vecDeltaX.at(k % vecDeltaX.size());
 				deltaY = vecDeltaY.at(k % vecDeltaX.size());
 
-				if (row + deltaX < 0 || row + deltaX > grayscaleInput.rows)
+				//If the pixel is outside the image
+				if (row + deltaX < 0 || row + deltaX > grayscalePic.rows)
 					continue;
 
-				if (col + deltaY < 0 || col + deltaY > grayscaleInput.cols)
+				if (col + deltaY < 0 || col + deltaY > grayscalePic.cols)
 					continue;
 
-				uchar valDeltaPixel = grayscaleInput.at<uchar>(col + deltaY, row + deltaX);
+				//Value of the compared pixel
+				uchar valDeltaPixel = grayscalePic.at<uchar>(row + deltaX, col + deltaY);
 
+				//If brighter increment the counter and set darker pixels counter to 0
 				if (valCenterPixel + deltaVal > valDeltaPixel)
 				{
 					nbContiguousBrighter++;
 					nbContiguousDarker = 0;
 				}
 
+				//If darker increment the counter and set brighter pixels counter to 0
 				if (valCenterPixel - deltaVal < valDeltaPixel)
 				{
 					nbContiguousDarker++;
 					nbContiguousBrighter = 0;
 				}
 
+				//Save the pixel as a corner
 				if (nbContiguousBrighter >= neededContiguous || nbContiguousDarker >= neededContiguous)
 				{
-					foundCorner = true;
-					vecCorners.push_back(Point2d(row + deltaX, col + deltaY));
-					circle(originalPic, Point(row + deltaX, col + deltaY), 5, Scalar(0, 0, 255), 1, 8, 0);
+					cornerPoint cp = {Point(col, row), false};
+					vecCorners.push_back(cp);
+
 					break;
 				}
 			}
 		}
 	}
+	return vecCorners;
+}
 
-	imshow("Center", originalPic);
+Mat drawCorners(Mat pic, vector<cornerPoint> corners)
+{
+	Mat picWithCorners = pic.clone();
+	for (unsigned int i = 0; i < corners.size(); i++)
+	{
+		Point corner = corners.at(i).point;
+		circle(picWithCorners, corner, 1, Scalar(0, 0, 255), 1, 8, 0);
+	}
+	return picWithCorners;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 4)
+	{
+		cout << "Utilisation : ./fast pic1 pic2 output" << endl;
+		return -1;
+	}
+
+	//const char *output = argv[3];
+
+	vector<Mat> originalPictures;
+	vector<Mat> picWithCircles;
+	vector<Mat> grayscalePictures;
+
+	Mat firstOriginalPic = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	Mat secondOriginalPic = imread(argv[2], CV_LOAD_IMAGE_COLOR);
+
+	Mat firstGrayscalePic = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	Mat secondGrayscalePic = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+
+	for (unsigned int i = 0; i < originalPictures.size(); i++)
+	{
+		if (!originalPictures.at(i).data || !grayscalePictures.at(i).data) // Check for invalid input
+		{
+			cout << "Could not open or find one of the given image" << endl;
+			return -1;
+		}
+	}
+
+	vector<cornerPoint> cornersFirstPic = getCorners(firstGrayscalePic);
+	vector<cornerPoint> cornersSecondPic = getCorners(secondGrayscalePic);
+
+	Mat firstPicWithCorners = drawCorners(firstOriginalPic, cornersFirstPic);
+	Mat secondPicWithCorners = drawCorners(secondOriginalPic, cornersSecondPic);
+
+	Mat bothOriginalPic(firstOriginalPic.rows + secondOriginalPic.rows, firstOriginalPic.cols + secondOriginalPic.cols, firstOriginalPic.type());
+	hconcat(firstOriginalPic, secondOriginalPic, bothOriginalPic);
+
+	int c = 0;
+	for (unsigned int i = 0; i < cornersFirstPic.size(); i++)
+	{
+		Point comparedPixel = cornersFirstPic.at(i).point;
+		int bestPixelIndex = 0;
+		int min = INT_MAX;
+		for (unsigned int j = 0; j < cornersSecondPic.size(); j++)
+		{
+			Point pixelToCompare = cornersSecondPic.at(j).point;
+
+			//Looking for a 3x3 pixel around each corner
+			int SSD = 0;
+
+			//3x3 area around both pixels
+			for (int k = -1; k <= 1; k++)
+			{
+				if (comparedPixel.x + k >= firstGrayscalePic.rows || comparedPixel.x + k < 0)
+					continue;
+
+				if (pixelToCompare.x + k >= secondGrayscalePic.rows || pixelToCompare.x + k < 0)
+					continue;
+
+				for (int m = -1; m <= 1; m++)
+				{
+					if (comparedPixel.y + m >= firstGrayscalePic.cols || comparedPixel.y + m < 0)
+						continue;
+
+					if (pixelToCompare.y + m >= secondGrayscalePic.cols || pixelToCompare.y + m < 0)
+						continue;
+
+					SSD += pow(firstGrayscalePic.at<uchar>(comparedPixel.x + k, comparedPixel.y + m) - secondGrayscalePic.at<uchar>(pixelToCompare.x + k, pixelToCompare.y + m), 2);
+
+					if (SSD < min && !cornersSecondPic.at(j).matched)
+					{
+						min = SSD;
+						bestPixelIndex = j;
+					}
+				}
+			}
+		}
+
+		if (!cornersSecondPic.at(bestPixelIndex).matched)
+		{
+			cout << "compared " << comparedPixel.x << " " << comparedPixel.y << endl;
+			cout << "matched " << cornersSecondPic.at(bestPixelIndex).point.x << " " << cornersSecondPic.at(bestPixelIndex).point.y << endl;
+			cornersSecondPic.at(bestPixelIndex).matched = true;
+			Mat test = bothOriginalPic.clone();
+			line(test, Point(comparedPixel.x, comparedPixel.y), Point(cornersSecondPic.at(bestPixelIndex).point.x + secondOriginalPic.cols, cornersSecondPic.at(bestPixelIndex).point.y), Scalar(0, 0, 255), 1);
+			imshow("tamere", test);
+			waitKey(0);
+		}
+	}
+
+	imshow("Picture", bothOriginalPic);
+	waitKey(0);
+	// for (unsigned int i = 0; i < originalPictures.size(); i++)
+	// {
+	// 	imshow("Picture", originalPictures.at(i));
+	// 	waitKey(0);
+	// }
+
 	waitKey(0);
 
 	return 0;
